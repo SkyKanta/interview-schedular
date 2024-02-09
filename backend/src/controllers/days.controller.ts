496d2e71b7313b5674a2332bb326077faae2075e
Goktug YILDIZ <goktug.7tepe@gmail.com>
2024-02-09 15:51:28 -0800

    fixed the infinite loop issue inside nested loop

--- backend/src/controllers/days.controller.ts
+++ backend/src/controllers/days.controller.ts
@@ -5,26 +5,21 @@ const getDays = async (req: Request, res: Response): Promise<void> => {
   try {
     //get all days
     const allDays = await days.getDays();
-    console.log("we are here1 ");
 
     //logic to appointments: truepointments: trueeck the updates on each day
     for (let i = 0; i < allDays.length; i++) {
       const { id, appointments } = allDays[i];
       let spots = 0;
-      console.log("we are here2 ");
 
       //if there are no interview increase the spot
-      for (let j = 0; j < appointments.length; j) {
+      for (let j = 0; j < appointments.length; j++) {
         if (!appointments[j].interview) {
           spots++;
-          console.log(spots);
         }
-        console.log("we are here4");
       }
 
       // update the data that changed in client side
       allDays[i].spots = spots;
-      console.log("we are here5");
 
       // update the data in db
       await days.updateDays({
